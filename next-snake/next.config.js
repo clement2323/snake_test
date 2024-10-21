b48427eb20163e522151a41d2752907edfcfe2a4
clement <guillo.clement@gmail.com>
2024-10-20 21:21:56 -0400

    next config http https

---
+++ next-snake/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ]
+  }
+};
+
+export default nextConfig;
